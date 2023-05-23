13f7f2587f2b083daea43b5fced564b625e0a74b
riacd <1927208797@qq.com>
2023-05-23 10:48:52 +0800

    扫描界面输出信息更新

--- web/white_list/API.py
+++ web/white_list/API.py
@@ -1,11 +1,13 @@
+import threading
 import streamlit as st
 import pandas as pd
 import numpy as np
 import os
 import sys
+import time
 
 white_list = ['/VirEnv','/img.png','/global_API.py']
-
+text_output = []
 
 def add_Dir(dir: str):
     white_list.append(dir)
@@ -29,4 +31,20 @@ def scan(dir: str):
 	f = d.read()
 	print(f)
 	print("扫描完成!\n")
+	text_output.append("扫描完成!")
 	return f
+
+# #模拟扫描程序
+# def scan(*args):
+# 	print(args)
+# 	time.sleep(10)
+# 	text_output.append("扫描完成!")
+# 	print("扫描完成")
+
+
+def call_scan(dir: str):
+	print(dir)
+	t = threading.Thread(target=scan, args=str(dir))
+	t.start()
+	print("开始扫描!  请稍等...")
+	text_output.append("开始扫描!  请稍等...")
