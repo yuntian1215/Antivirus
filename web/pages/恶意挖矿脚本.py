e6fe3f9c18e748f97eceab67e5cc105493ecf866
yuntian1215 <yuntian1215@sjtu.edu.cn>
2023-05-27 16:38:19 +0800

    first part of cryptmining ui

--- web/pages/恶意挖矿脚本.py
+++ web/pages/恶意挖矿脚本.py
@@ -1,18 +1,37 @@
 import streamlit as st
 from crypto_jacking import API
 import pandas as pd
-from io import StringIO
 import global_API
+import streamlit as st
+import pandas as pd
+import time
+
+
+
+def update1(URL_search):
+    API.call_scan(URL_search)
+    st.experimental_rerun()
+
 
 def page_render(key_value):
-    function_tab, settings_tab, file_upload_tab,  = st.tabs(["挖矿脚本筛查", "实时更新", "HTML文件上传"])
+    function_tab, file_upload_tab, settings_tab,  = st.tabs(["挖矿脚本筛查", "HTML文件上传", "实时更新"])
+
     with function_tab:
-        URL_search = st.text_input(label='输入URL', key=key_value.value())
-        st.button("开始筛查", on_click=API.call_scan, args=[URL_search], key=key_value.value())
+        URL_search = st.text_input(label = '输入待检测的URL 👇')
+        print(URL_search)
+        st.write("You entered URL is: ", URL_search)
+        st.button("开始筛查", on_click=API.call_scan, args=[URL_search])
+        
         if API.flag:
             st.dataframe(pd.DataFrame({'输出信息': API.output_text}), width=700)
+
+            time.sleep(15)
+            st.experimental_rerun()
+        
+             
         else:
             st.write(API.failure_warning)
+
     with settings_tab:
         settings_file = st.file_uploader("Choose setting file", key=key_value.value())
         API.setting_text = st.text_area(label='setting_text', value=API.update_text, on_change=API.change_update_text, args=[API.setting_text], key=key_value.value())
@@ -22,21 +41,15 @@ def page_render(key_value):
 
     with file_upload_tab:
         uploaded_file = st.file_uploader("Choose a file", key=key_value.value())
+        html = uploaded_file.read().decode('utf-8')
+        
         if uploaded_file is not None:
-            # To read file as bytes:
-            bytes_data = uploaded_file.getvalue()
-            st.write(bytes_data)
+            st.button("开始筛查", on_click=API.call_scan, args=[URL_search], key=key_value.value())
+            
+        #     if API.flag:
+        #         st.dataframe(pd.DataFrame({'输出信息': API.output_text}), width=700)
+        #     else:
+        #         st.write(API.failure_warning)
 
-            # To convert to a string based IO:
-            stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-            st.write(stringio)
-
-            # To read file as string:
-            string_data = stringio.read()
-            st.write(string_data)
-
-            # # Can be used wherever a "file-like" object is accepted:
-            # dataframe = pd.read_csv(uploaded_file)
-            # st.write(dataframe)
-
+# threading.Thread(target=Listen).start()
 global_API.refresh_by_button(page_render)
